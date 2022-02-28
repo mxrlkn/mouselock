8bb68e20753bbfe7f558a63b1d787d65c422fca5
mxrlkn <mail@mxrlkn.com>
2022-02-28 22:09:29 +0100

    version builds

--- build.sh
+++ build.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
+# get latest tag starting with v
+VERSION=$(git tag -l 'v*' --sort=-v:refname | head -1)
+
+# build
 xcodebuild -quiet -configuration Release -target mouselock
 
+# set app version
+/usr/libexec/Plistbuddy -c "Set CFBundleVersion ${VERSION:1}" "build/Release/mouselock.app/Contents/info.plist"
+
+# clean dist
 rm -rf dist && mkdir dist
 
+# make dmg from app
 hdiutil create -fs HFS+ -srcfolder build/Release/mouselock.app -volname mouselock dist/mouselock.dmg
 
+# clean build
 rm -r build
