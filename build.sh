0f2912ce37aeb917af22f22ff3c2945390197cbc
mxrlkn <mail@mxrlkn.com>
2022-04-30 03:08:46 +0200

    fix build

--- build.sh
+++ build.sh
@@ -4,11 +4,11 @@ set -e
 # get latest tag starting with v
 VERSION=$(git tag -l 'v*' --sort=-v:refname | head -1)
 
+# set app version
+agvtool new-version ${VERSION:1}
+
 # build
 xcodebuild -quiet -configuration Release -target mouselock
-
-# set app version
-/usr/libexec/Plistbuddy -c "Set CFBundleVersion ${VERSION:1}" "build/Release/mouselock.app/Contents/info.plist"
 
 # clean dist
 rm -rf dist && mkdir dist
