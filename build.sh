b76030ba3f4afbfd48be70a62fc7142ebce3a4db
mxrlkn <mail@mxrlkn.com>
2022-11-06 15:48:40 +0100

    add app icon and rename to Mouselock.app (#10)

--- build.sh
+++ build.sh
@@ -8,13 +8,13 @@ VERSION=$(git tag -l 'v*' --sort=-v:refname | head -1)
 agvtool new-version ${VERSION:1}
 
 # build
-xcodebuild -quiet -configuration Release -target mouselock
+xcodebuild -quiet -configuration Release -target Mouselock
 
 # clean dist
 rm -rf dist && mkdir dist
 
 # make dmg from app
-hdiutil create -fs HFS+ -srcfolder build/Release/mouselock.app -volname mouselock dist/mouselock.dmg
+hdiutil create -fs HFS+ -srcfolder build/Release/Mouselock.app -volname Mouselock dist/Mouselock.dmg
 
 # clean build
 rm -r build
